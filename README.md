45984f4df3d026c11e616253bb1d164850914ba8
Ruchila Baruah <ruchilabaruah92@gmail.com>
2026-02-20 13:56:43 +0100

    implement functionality to add sticky note at correct position

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Steps:
 
 1. Render a container area boundary for sticky notes
 2. Display a fixed add button at right bottom corner of the screen which will add a new note when clicked.
+3. Define a palette of pastel colors and a function to assign random colors to new notes.
+4. Add a function to calculate position of a new sticky note.
+5. Add note to the list of notes using the above two functions.
--- src/StickyNotepad.jsx
+++ src/StickyNotepad.jsx
@@ -1,11 +1,93 @@
-import React from "react";
+import React, { useState } from "react";
 import { Plus, X } from "lucide-react";
 import "./App.css";
 
+const COLORS = [
+  "#FFFA65",
+  "#FF9AA2",
+  "#FFB7B2",
+  "#FFDAC1",
+  "#E2F0CB",
+  "#B5EAD7",
+  "#C7CEEA",
+];
+
+const NOTE_WIDTH = 200;
+const NOTE_HEIGHT = 150;
+const GAP = 15;
+const COLUMNS = 3;
+
+function getRandomColor() {
+  return COLORS[Math.floor(Math.random() * COLORS.length)];
+}
+
+function getGridPosition(index) {
+  const col = index % COLUMNS;
+  const row = Math.floor(index / COLUMNS);
+
+  return {
+    x: col * (NOTE_WIDTH + GAP), // Number of columns before it * (One sticky note width + Gap)
+    y: row * (NOTE_WIDTH + GAP),
+  };
+}
+
 function StickyNotepad() {
+  const [notes, setNotes] = useState([]);
+
+  const addNote = () => {
+    const occupiedPositions = new Set(
+      notes.map((note) => `${note.position.x}, ${note.position.y}`),
+    );
+
+    let index = 0;
+    let position = null;
+
+    while (true) {
+      const pos = getGridPosition(index);
+      const key = `${pos.x}, ${pos.y}`;
+      if (!occupiedPositions.has(key)) {
+        position = pos;
+        break;
+      }
+      index++;
+    }
+    const newNote = {
+      id: Date.now(),
+      text: "",
+      isDragging: false,
+      color: getRandomColor(),
+      position,
+    };
+
+    setNotes((prev) => [...prev, newNote]);
+  };
   return (
     <div className="container">
-      <button className="add-note-btn">
+      <div style={{ position: "relative", width: "100%" }}>
+        {notes.map(({ id, color, position }, index) => (
+          <div
+            key={id}
+            className="note"
+            style={{
+              backgroundColor: color,
+              zIndex: index + 1,
+              left: position.x,
+              top: position.y,
+            }}
+          >
+            <button className="close-btn">
+              <X className="icon-close" />
+            </button>
+            <textarea className="note-textarea" />
+          </div>
+        ))}
+      </div>
+
+      <button
+        className="add-note-btn"
+        onClick={addNote}
+        style={{ zIndex: 1000 }}
+      >
         <Plus className="icon-add" />
       </button>
     </div>
